cb5641f94d646f3fb9f8d438e35133471d060095
Ayi Muhammad Iqbal Nasuha <bonk007@gmail.com>
2025-03-12 23:46:00 +0700

    fix: under development dashboard

--- resources/views/components/dashboard/pages/home.blade.php
+++ resources/views/components/dashboard/pages/home.blade.php
@@ -1,5 +1,8 @@
 <x-dashboard.layout>
-    <div>
+    <div class="h-full flex justify-center items-center">
 {{--        <livewire:dashboard.eye-tracker-downloader />--}}
+        <div class="border-2 rounded px-8 py-4 border-red-400">
+            Cognitive Load Dashboard is being under development
+        </div>
     </div>
 </x-dashboard.layout>
